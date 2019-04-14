c55aad182021cd1dc2c817aebc95fd2db855def3
zliden01 <zliden01@protonmail.com>
2019-04-14 16:05:37 +0300

    feat: solved first task array

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -23,7 +23,8 @@
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
 function findElement(arr, value) {
-   throw new Error('Not implemented');
+   //throw new Error('Not implemented');
+   return arr.indexOf(value);
 }
 
 /**
